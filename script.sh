0edcfd5047b431b2bc4a382998ef9a47163bfa4b
Krasi Marinov <47538690+kmarinov7@users.noreply.github.com>
2021-07-20 12:14:03 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-  
+
+# Convert the input names to final names removing duplications and capital letters
 awk '!seen[$0]++' names > removed_duplicates && awk '{print tolower($0)}' removed_duplicates > removed_cl && awk '!seen[$0]++' removed_cl > final_names
+# Remove not needed files
 rm removed_duplicates removed_cl
+# For each line in final names file create a directory. For names with whitespace swap it with underscore
 input="final_names"
 while IFS= read -r line
 do
